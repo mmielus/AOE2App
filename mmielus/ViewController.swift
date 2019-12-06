5aafa1f07ad41e93ba4a0111f37fe41b41ca7411
Guest User <Guest@imac-05.local>
2019-12-06 10:26:03 +0100

    initial

---
+++ mmielus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mmielus
+//
+//  Created by Guest User on 06.12.2019.
+//  Copyright © 2019 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
